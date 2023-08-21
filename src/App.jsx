72624356a70666a7dfb23dc9338a0afa5e0c53d1
Esmir Roque Castellano <74161172+castellano17@users.noreply.github.com>
2023-08-20 21:44:33 -0600

    solutions error navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { NavbarLayout } from "./components/layout/navbar";
+import { NavbarLayout } from "./components/layout/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
